befecfd11bb793439e7a39b371fb709cea0c97d4
Sinyak31 <Sindyai31@gmail.com>
2024-05-29 22:26:59 +0400

    -adds an initialization block to the service -changing the getListCar() method in the service

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,22 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
 
-import java.util.List;
-import java.util.stream.Stream;
 
 @Controller
 public class CarController {
+    private final CarService carService;
+
     @Autowired
-    private CarService carService;
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
-    public String showListCar(@RequestParam(value = "count",defaultValue = "5") int count, Model model) {
-        Stream<Car> stream = carService.getListCar().stream();
-        List<Car> carList = stream.limit(count).toList();
-        model.addAttribute("carList",carList);
+    public String showListCar(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        model.addAttribute("carList", carService.getListCar(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -5,23 +5,27 @@ import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+
 @Service
 public class CarServiceImp implements CarService {
+    private List<Car> carList;
 
-    @Override
-    public List<Car> getListCar() {
-        List<Car> carList = new ArrayList<>();
-        Car car1 = new Car("Lada","Red");
-        Car car2 = new Car("BMW","Green");
-        Car car3 = new Car("Kamaz","Black");
-        Car car4 = new Car("Audi","White");
-        Car car5 = new Car("Lexus","Pink");
+    {
+        carList = new ArrayList<>();
+        Car car1 = new Car("Lada", "Red");
+        Car car2 = new Car("BMW", "Green");
+        Car car3 = new Car("Kamaz", "Black");
+        Car car4 = new Car("Audi", "White");
+        Car car5 = new Car("Lexus", "Pink");
         carList.add(car1);
         carList.add(car2);
         carList.add(car3);
         carList.add(car4);
         carList.add(car5);
-        return carList;
+    }
+
+    @Override
+    public List<Car> getListCar(int count) {
+        return carList.stream().limit(count).toList();
     }
 }
